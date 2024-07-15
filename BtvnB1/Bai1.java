a29f55d7bf2ae98fc3025bd487d246458210df66
HuyBeros <huyblue181005@gmail.com>
2024-07-15 21:38:37 +0700

    BtvnB1

---
+++ BtvnB1/Bai1.java
@@ -0,0 +1,41 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class Bai1 {
+    public static void main(String[] args){
+        /*
+        cau1:  in bien ra man hinh;
+        float PI=3.14135F;
+        int a=1000;
+        float d=10.3453453435345F;
+        char b='k';
+        System.out.println("gia tri cua a la: " + a);
+        System.out.println(b);
+        System.out.printf("gia tri cua Pi va d la: %.2f %.10f", PI, d );
+        System.out.println("");
+        System.out.println("gia tri cua bien la: " + a + " " + b);*/
+        Scanner sc= new Scanner(System.in);
+        /*int a;
+        char kiTu= sc.nextLine().charAt(0);
+        System.out.println(kiTu); nhap ki tu va in ra man hinh
+        /*a = sc.nextInt();
+        System.out.println(a); 
+        nhap so lieu tu ban phim va in ra man hinh
+        float b = sc.nextFloat();
+        System.out.println(b);*/
+       int a= sc.nextInt();
+       System.out.println("nhap vao so x: " + a );
+        System.out.println("Hello World!");
+        System.out.println("28tech C++ programming");
+        
+    }
+}

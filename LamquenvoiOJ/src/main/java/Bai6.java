9c2a35a4895fabdd1bdb282acffbb37fb14d09fe
HuyBeros <huyblue181005@gmail.com>
2024-07-16 17:41:19 +0700

    LamquenvoiOJ

--- LamquenvoiOJ/src/main/java/Bai6.java
+++ LamquenvoiOJ/src/main/java/Bai6.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -8,5 +11,15 @@
  * @author Admin
  */
 public class Bai6 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double x,ceilx,floorx,roundx;
+        x = sc.nextDouble();
+        ceilx=Math.ceil(x);
+        floorx=Math.floor(x);
+        roundx=Math.round(x);
+        System.out.printf("%.0f%n",ceilx);
+        System.out.printf("%.0f%n",floorx);
+        System.out.printf("%.0f%n",roundx);
+    }
 }
---
+++ LamquenvoiOJ/src/main/java/Bai7.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class Bai7 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        long n = sc.nextLong();
+        System.out.println(n%10 + "\n" + n%100);
+        
+    }
+}
